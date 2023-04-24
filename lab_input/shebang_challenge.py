89c013ea2355191d6e1228d6c7b74cfd437c7429
CWise423 <calebdwise@gmail.com>
2023-04-24 22:32:00 +0000

    Challenge attempt

---
+++ lab_input/shebang_challenge.py
@@ -0,0 +1,10 @@
+#!/user/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - display data to std out"""
+
+def main():
+    name = input("What is your name?")
+    day = input("What day of the week is it?")
+    print(f"Hello, {name} Happy {day}!")
+
+    

39c43f7079df7bd442a45f08ce83e63bd0da5dfe
CWise423 <calebdwise@gmail.com>
2023-04-25 20:12:01 +0000

    join and split

--- lab_input/shebang_challenge.py
+++ lab_input/shebang_challenge.py
@@ -1,10 +1,9 @@
 #!/user/bin/env python3
-"""Alta3 Research | RZFeeser
-   print() - display data to std out"""
+
 
 def main():
-    name = input("What is your name?")
-    day = input("What day of the week is it?")
+    name = input("What is your name?\n> " )
+    day = input("What day of the week is it?\n> ")
     print(f"Hello, {name} Happy {day}!")
 
     
